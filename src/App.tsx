8e992f0b69b8ffa1dbed472ee896d076b77ff739
Zaranasirova <zerifenesirova784@gmail.com>
2024-12-17 16:22:51 +0400

    Add final touches to banner section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
