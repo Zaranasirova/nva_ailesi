1573848666dc54aa62a2fa58c5beda7890070694
Zaranasirova <zerifenesirova784@gmail.com>
2024-12-17 16:56:04 +0400

    Fix banner image alignment and text position

--- src/pages/sections/Banner.tsx
+++ src/pages/sections/Banner.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+import banner from "../../assets/image/ikea_1.jpg";
+
 const Banner = () => {
   return (
     <section className="banner">
       <div className="container">
-        <div className="banner-item"></div>
+        <Link to={"/"} className="banner-item">
+          <div className="banner-image">
+            {" "}
+            <img src={banner} alt="banner" />
+          </div>
+        </Link>
       </div>
     </section>
   );
