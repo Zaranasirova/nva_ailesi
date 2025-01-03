609c80dcb88548c99b7018a9690658f6f27d371d
Zaranasirova <zerifenesirova784@gmail.com>
2025-01-03 13:09:13 +0400

    Complete most-read section structure

--- src/pages/sections/PopularScenarios.tsx
+++ src/pages/sections/PopularScenarios.tsx
@@ -13,15 +13,99 @@ const PopularScenarios = () => {
               <img src={image} alt="most-read" />
             </div>
             <div className="post-info">
-            <span className="read-year">2024</span>
-            <span className="read-comment">
-              <img src={comment} alt="comment-svg" className="comment-icon" />
-              <span className="comment-number">0</span>
-            </span>
-            <span className="read-stars">
-              <img src={stars} alt="stars-svg" className="star-image" />
-              <span className="stars-number">8.2</span>
-            </span>
+              <div className="movie-meta">
+                <span className="read-year">2024</span>
+                <span className="read-comment">
+                  <img
+                    src={comment}
+                    alt="comment-svg"
+                    className="comment-icon"
+                  />
+                  <span className="comment-number">0</span>
+                </span>
+                <span className="read-stars">
+                  <img src={stars} alt="stars-svg" className="star-image" />
+                  <span className="stars-number">8.2</span>
+                </span>
+              </div>
+              <div className="movie-title">
+                <h2>Lorem Ipsum</h2>
+              </div>
+            </div>
+          </div>
+          <div className="most-read">
+            <div className="most-read-image">
+              <img src={image} alt="most-read" />
+            </div>
+            <div className="post-info">
+              <div className="movie-meta">
+                <span className="read-year">2024</span>
+                <span className="read-comment">
+                  <img
+                    src={comment}
+                    alt="comment-svg"
+                    className="comment-icon"
+                  />
+                  <span className="comment-number">0</span>
+                </span>
+                <span className="read-stars">
+                  <img src={stars} alt="stars-svg" className="star-image" />
+                  <span className="stars-number">8.2</span>
+                </span>
+              </div>
+              <div className="movie-title">
+                <h2>Lorem Ipsum</h2>
+              </div>
+            </div>
+          </div>
+          <div className="most-read">
+            <div className="most-read-image">
+              <img src={image} alt="most-read" />
+            </div>
+            <div className="post-info">
+              <div className="movie-meta">
+                <span className="read-year">2024</span>
+                <span className="read-comment">
+                  <img
+                    src={comment}
+                    alt="comment-svg"
+                    className="comment-icon"
+                  />
+                  <span className="comment-number">0</span>
+                </span>
+                <span className="read-stars">
+                  <img src={stars} alt="stars-svg" className="star-image" />
+                  <span className="stars-number">8.2</span>
+                </span>
+              </div>
+              <div className="movie-title">
+                <h2>Lorem Ipsum</h2>
+              </div>
+            </div>
+          </div>
+          <div className="most-read">
+            <div className="most-read-image">
+              <img src={image} alt="most-read" />
+            </div>
+            <div className="post-info">
+              <div className="movie-meta">
+                <span className="read-year">2024</span>
+                <span className="read-comment">
+                  <img
+                    src={comment}
+                    alt="comment-svg"
+                    className="comment-icon"
+                  />
+                  <span className="comment-number">0</span>
+                </span>
+                <span className="read-stars">
+                  <img src={stars} alt="stars-svg" className="star-image" />
+                  <span className="stars-number">8.2</span>
+                </span>
+              </div>
+              <div className="movie-title">
+                <h2>Lorem Ipsum</h2>
+              </div>
             </div>
           </div>
         </div>
