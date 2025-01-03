e0bd4274369ddbccc42bf91b8b1f790fba987728
Zaranasirova <zerifenesirova784@gmail.com>
2025-01-03 14:07:57 +0400

    Most read section SCSS layout coding

--- src/pages/sections/PopularScenarios.tsx
+++ src/pages/sections/PopularScenarios.tsx
@@ -15,7 +15,8 @@ const PopularScenarios = () => {
             <div className="post-info">
               <div className="movie-meta">
                 <span className="read-year">2024</span>
-                <span className="read-comment">
+              <div className="comment-and-stars">
+              <span className="read-comment">
                   <img
                     src={comment}
                     alt="comment-svg"
@@ -27,6 +28,7 @@ const PopularScenarios = () => {
                   <img src={stars} alt="stars-svg" className="star-image" />
                   <span className="stars-number">8.2</span>
                 </span>
+              </div>
               </div>
               <div className="movie-title">
                 <h2>Lorem Ipsum</h2>
@@ -40,7 +42,8 @@ const PopularScenarios = () => {
             <div className="post-info">
               <div className="movie-meta">
                 <span className="read-year">2024</span>
-                <span className="read-comment">
+              <div className="comment-and-stars">
+              <span className="read-comment">
                   <img
                     src={comment}
                     alt="comment-svg"
@@ -52,6 +55,7 @@ const PopularScenarios = () => {
                   <img src={stars} alt="stars-svg" className="star-image" />
                   <span className="stars-number">8.2</span>
                 </span>
+              </div>
               </div>
               <div className="movie-title">
                 <h2>Lorem Ipsum</h2>
@@ -65,7 +69,8 @@ const PopularScenarios = () => {
             <div className="post-info">
               <div className="movie-meta">
                 <span className="read-year">2024</span>
-                <span className="read-comment">
+              <div className="comment-and-stars">
+              <span className="read-comment">
                   <img
                     src={comment}
                     alt="comment-svg"
@@ -77,6 +82,7 @@ const PopularScenarios = () => {
                   <img src={stars} alt="stars-svg" className="star-image" />
                   <span className="stars-number">8.2</span>
                 </span>
+              </div>
               </div>
               <div className="movie-title">
                 <h2>Lorem Ipsum</h2>
@@ -90,7 +96,8 @@ const PopularScenarios = () => {
             <div className="post-info">
               <div className="movie-meta">
                 <span className="read-year">2024</span>
-                <span className="read-comment">
+              <div className="comment-and-stars">
+              <span className="read-comment">
                   <img
                     src={comment}
                     alt="comment-svg"
@@ -102,6 +109,34 @@ const PopularScenarios = () => {
                   <img src={stars} alt="stars-svg" className="star-image" />
                   <span className="stars-number">8.2</span>
                 </span>
+              </div>
+              </div>
+              <div className="movie-title">
+                <h2>Lorem Ipsum</h2>
+              </div>
+            </div>
+          </div>
+          <div className="most-read">
+            <div className="most-read-image">
+              <img src={image} alt="most-read" />
+            </div>
+            <div className="post-info">
+              <div className="movie-meta">
+                <span className="read-year">2024</span>
+              <div className="comment-and-stars">
+              <span className="read-comment">
+                  <img
+                    src={comment}
+                    alt="comment-svg"
+                    className="comment-icon"
+                  />
+                  <span className="comment-number">0</span>
+                </span>
+                <span className="read-stars">
+                  <img src={stars} alt="stars-svg" className="star-image" />
+                  <span className="stars-number">8.2</span>
+                </span>
+              </div>
               </div>
               <div className="movie-title">
                 <h2>Lorem Ipsum</h2>
