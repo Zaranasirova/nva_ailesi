5bd0d2d8ef8d53b91401168616750802c3c24ff1
Zaranasirova <zerifenesirova784@gmail.com>
2025-01-09 18:14:29 +0400

    feedback-left CSS styling

--- src/pages/sections/UserReviewsSection.tsx
+++ src/pages/sections/UserReviewsSection.tsx
@@ -11,46 +11,62 @@ const UserReviewsSection = () => {
       <div className="userReviews">
         <h2 className="head-title">İstifadəçi rəyləri</h2>
         <div className="customer-feed-back-box">
-          <div className="container">
+          <div className="container customer-feedback">
             <div className="feedback-left">
-              <h2 className="feedback-title">
+              <p className="feedback-title">
                 İstifadəçilər <span className="feedback-title-span">bizim</span>{" "}
                 haqqımızda nə düşünür?
-              </h2>
+              </p>
               <p className="feedback-text-prompt">
                 Zəhmət olmasa dəyərli fikirlərinizi bizimlə bölüşün.
               </p>
-              <div className="feedback-list">
-                <ul>
-                  <li>
+              <div className="feedback-list d-flex">
+                <ul className="feedback-list__images">
+                  <li className="feedback-list__item">
                     <Link to={"/"}>
-                      <img src={ellipse_image1} alt="" />
+                      <img
+                        src={ellipse_image1}
+                        alt=""
+                        className="feedback-list__image"
+                      />
                     </Link>
                   </li>
-                  <li>
+                  <li className="feedback-list__item">
                     <Link to={"/"}>
-                      <img src={ellipse_image2} alt="" />
+                      <img
+                        src={ellipse_image2}
+                        alt=""
+                        className="feedback-list__image"
+                      />
                     </Link>
                   </li>
-                  <li>
+                  <li className="feedback-list__item">
                     <Link to={"/"}>
-                      <img src={ellipse_image3} alt="" />
+                      <img
+                        src={ellipse_image3}
+                        alt=""
+                        className="feedback-list__image"
+                      />
                     </Link>
                   </li>
-                  <li>
+                  <li className="feedback-list__item">
                     <Link to={"/"}>
-                      <img src={ellipse_image4} alt="" />
+                      <img
+                        src={ellipse_image4}
+                        alt=""
+                        className="feedback-list__image"
+                      />
                     </Link>
                   </li>
                 </ul>
-                <span>20+ Rəy</span>
+                <span className="feedback-list__count">20+ Rəy</span>
               </div>
             </div>
             <div className="feedback-right">
               <div className="latest-feedback-profile">
                 <div className="feedback-profile">
                   <div className="latest-avatar">
-                    <img src="" alt="" />
+                    <img src={ellipse_image1} alt="" />
                   </div>
                   <h3 className="latest-name">Aysu Jafar</h3>
                   <span className="latest-date">12/12/24</span>
