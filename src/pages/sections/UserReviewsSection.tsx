e1157341eb92efec3a90e1bfc6828be5a31571bd
Zaranasirova <zerifenesirova784@gmail.com>
2025-01-09 16:13:28 +0400

    feat: Added SCSS for UserReviews section

--- src/pages/sections/UserReviewsSection.tsx
+++ src/pages/sections/UserReviewsSection.tsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 
 const UserReviewsSection = () => {
   return (
-    <section className="userReviews">
-      <div className="container">
+    <section className="userReviews-section">
+      <div className="userReviews">
         <h2 className="head-title">İstifadəçi rəyləri</h2>
-        <div className="customer-feed-back-box">
+        <div className="customer-feed-back-box" style={{background:"red"}}>
+          <div className="container">
           <div className="feedback-left">
             <h2 className="feedback-title">
               İstifadəçilər <span className="feedback-title-span">bizim</span> haqqımızda nə düşünür?
@@ -20,10 +21,15 @@ const UserReviewsSection = () => {
                 <li><Link to={"/"}><img src="" alt="" /></Link></li>
                 <li><Link to={"/"}><img src="" alt="" /></Link></li>
               </ul>
-              
+              <span>20+ Rəy</span>
             </div>
           </div>
-          <div className="feedback-right"></div>
+          <div className="feedback-right">
+            <div className="latest-feedback-profile">
+              <div className="latest-avatar"><img src="" alt="" /></div>
+            </div>
+          </div>
+          </div>
         </div>
       </div>
     </section>
