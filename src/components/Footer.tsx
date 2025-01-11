cc3d5117eb601f03428e387daf6d9a995ba08799
Zaranasirova <zerifenesirova784@gmail.com>
2025-01-11 16:49:30 +0400

    create footer scss

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,69 @@
-
+import { Link } from "react-router-dom";
+import logo from "../assets/image/nicat logo expand2-01 1.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="container footer-wrapper">
+        <div className="footer-logo-country-text">
+          <div className="fooer-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <p className="country-text">
+            Ölkənin ilk <span>özəl</span> və <span>#dahaFərqli</span>ssenari
+            platformu
+          </p>
+        </div>
+        <nav className="footer-navbar">
+          <ul className="footer-navbar-list">
+            <li className="footer-list">
+              <Link to={"/"}>Haqqımızda</Link>
+            </li>
+            <li className="footer-list">
+              <Link to={"/"}>Ssenarilər</Link>
+            </li>
+            <li className="footer-list">
+              <Link to={"/"}>Ssenaristlər</Link>
+            </li>
+            <li className="footer-list">
+              <Link to={"/"}>Əlaqə</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="footer-contact-links">
+          <ul className="contact-list">
+            <li className="contact-list-item">
+              <Link to={"/"}>
+                <span></span>
+                <span>nvailesi.az@gmail.com</span>
+              </Link>
+            </li>
+            <li className="contact-list-item">
+              <Link to={"/"}>
+                <span></span>
+                <span>(+994) 00 000 00 00</span>
+              </Link>
+            </li>
+            <li className="contact-list-item">
+              <Link to={"/"}>
+                <span></span>
+                <span>nvailesi</span>
+              </Link>
+            </li>
+            <li className="contact-list-item">
+              <Link to={"/"}>
+                <span></span>
+                <span>nvailesi</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="copyWrite">
+            <p>© 2024 Bütün hüquqlar qorunur</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
